f54b0d4e9382ab4550911abd81d12a74b3735f82
Liebling0825 <114152019+Liebling0825@users.noreply.github.com>
2022-09-23 17:50:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ojYPa6fB443UUoCB8WESkwTdTd2g",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ctQPNtxP804zGDMG_EUtPYcUKshnMU6z1IScVY9H0bU",
+      useTemplateId: "HxBwcfJBdgGasK--MebT_3A1IWixZbP7EHo1KNDLK3M",
       // 所在省份
       province: "湖南",
       // 所在城市
